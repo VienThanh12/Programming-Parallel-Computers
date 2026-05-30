d665dc9b572a05c27415443c74b21eb18e41c988
VienThanh12 <vienha000@gmail.com>
2026-05-30 20:19:01 +0300

    sol for mf1

--- exercises/mf/mf1/mf1/mf.cc
+++ exercises/mf/mf1/mf1/mf.cc
@@ -7,5 +7,30 @@ This is the function you need to implement. Quick reference:
   max(x-hx, 0) <= a < min(x+hx+1, nx), max(y-hy, 0) <= b < min(y+hy+1, ny)
   in out[x + y*nx].
 */
+#include<math.h>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
 void mf(int ny, int nx, int hy, int hx, const float *in, float *out) {
+
+  for (int y = 0; y < ny; y++) {
+    for (int x = 0; x < nx; x++) {
+      int count = 0;
+      std::vector<float> neighbors;
+      for (int b = std::max(y - hy, 0); b < std::min(y + hy + 1, ny); b++) {
+          for(int a = std::max(x - hx, 0); a < std::min(x + hx + 1, nx); a++) {
+              neighbors.push_back(in[a + b * nx]);
+              count++;
+          }
+      }
+      std::nth_element(neighbors.begin(), neighbors.begin() + count / 2, neighbors.end());
+      if(count % 2 == 0) {
+          std::nth_element(neighbors.begin(), neighbors.begin() + count / 2 - 1, neighbors.end());
+          out[x + y * nx] = (neighbors[count / 2] + neighbors[count / 2 - 1]) / 2.0f;
+      } else {
+          out[x + y * nx] = neighbors[count / 2];
+      }
+    }
+  }
 }
