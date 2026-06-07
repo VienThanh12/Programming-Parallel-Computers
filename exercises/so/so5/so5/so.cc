8a8c70411bcb315221a53c8aa001f4c27553c6ec
VienThanh12 <vienha000@gmail.com>
2026-06-07 12:38:10 +0300

    sol for so5

--- exercises/so/so5/so5/so.cc
+++ exercises/so/so5/so5/so.cc
@@ -1,9 +1,59 @@
+#include <iostream>
+#include <vector>
 #include <algorithm>
+#include <omp.h> 
 
 typedef unsigned long long data_t;
 
+int partition(data_t* data, int left, int right) {
+    int mid = left + (right - left) / 2;
+    if (data[mid] < data[left]) std::swap(data[left], data[mid]);
+    if (data[right] < data[left]) std::swap(data[left], data[right]);
+    if (data[right] < data[mid]) std::swap(data[mid], data[right]);
+    
+    std::swap(data[mid], data[right]); 
+    data_t pivot = data[right];
+    
+    int i = left - 1;
+    for (int j = left; j < right; ++j) {
+        if (data[j] < pivot) {
+            ++i;
+            std::swap(data[i], data[j]);
+        }
+    }
+    std::swap(data[i + 1], data[right]);
+    return i + 1;
+}
+
+void quicksort_helper(data_t* data, int left, int right) {
+    if (left >= right) {
+        return;
+    }
+
+    if (right - left < 10000) {
+        std::sort(data + left, data + right + 1);
+        return;
+    }
+
+    int pivot_idx = partition(data, left, right);
+
+    #pragma omp task shared(data)
+    quicksort_helper(data, left, pivot_idx - 1);
+
+    #pragma omp task shared(data)
+    quicksort_helper(data, pivot_idx + 1, right);
+
+    #pragma omp taskwait
+}
+
 void psort(int n, data_t *data) {
-    // FIXME: Implement a more efficient parallel sorting algorithm for the CPU,
-    // using the basic idea of quicksort.
-    std::sort(data, data + n);
+    if (n <= 1) return;
+
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+            quicksort_helper(data, 0, n - 1);
+        }
+    }
 }
